cf800dce9a342b8f8f0d2913d0b66049ccd9a995
José Eduardo <157645066+joeduardo10@users.noreply.github.com>
2025-04-15 21:48:32 -0300

    adiciona função para remover conexões de usuários e atualiza a exportação

--- servidor/utils/conexoesDocumentos.js
+++ servidor/utils/conexoesDocumentos.js
@@ -1,5 +1,6 @@
 const conexoesDocumentos = [];   
 
+
 function adicionarConexao(conexao){
     conexoesDocumentos.push(conexao);
     
@@ -11,4 +12,14 @@ function obterUsuariosNoDocumento(nomeDocumento){
     .map((conexao) => conexao.nomeUsuario)
 }
 
-export {adicionarConexao, obterUsuariosNoDocumento };
+function removerConexao(nomeDocumento,nomeUsuario){
+    const indice = conexoesDocumentos.findIndex((conexao)=>{
+        return (conexao.nomeDocumento === nomeDocumento && conexao.nomeUsuario === nomeUsuario);
+    });
+    if(indice !== -1){
+        conexoesDocumentos.splice(indice,1);
+    }
+    console.log(conexoesDocumentos);  
+}
+
+export {adicionarConexao, obterUsuariosNoDocumento, removerConexao};
