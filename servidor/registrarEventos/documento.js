abfbafff3ca397548b13642b07e2ae8f2185346d
José Eduardo <157645066+joeduardo10@users.noreply.github.com>
2025-04-15 21:48:05 -0300

    adiciona lógica para remover conexões de usuários e atualiza eventos de documento

--- servidor/registrarEventos/documento.js
+++ servidor/registrarEventos/documento.js
@@ -3,12 +3,10 @@ import {
   encontrarDocumento,
   excluirDocumento,
 } from "../db/documentosDb.js";
-import { adicionarConexao, obterUsuariosNoDocumento } from "../utils/conexoesDocumentos.js";
+import { adicionarConexao, obterUsuariosNoDocumento, removerConexao } from "../utils/conexoesDocumentos.js";
 
 function registrarEventosDocumento(socket, io) {
   socket.on("selecionar_documento", async ({ nomeDocumento, nomeUsuario }, devolverTexto) => {
-    
-
     
     const documento = await encontrarDocumento(nomeDocumento);
     
@@ -22,23 +20,34 @@ function registrarEventosDocumento(socket, io) {
       io.to(nomeDocumento).emit("usuarios_no_documento", usuariosNoDocumento);
       devolverTexto(documento.texto);
     }
-  });
+    socket.on("texto_editor", async ({ texto, nomeDocumento }) => {
+      const atualizacao = await atualizaDocumento(nomeDocumento, texto);
+  
+      if (atualizacao.modifiedCount) {
+        socket.to(nomeDocumento).emit("texto_editor_clientes", texto);
+      }
+    });
+  
+    socket.on("excluir_documento", async (nome) => {
+      const resultado = await excluirDocumento(nome);
+  
+      if (resultado.deletedCount) {
+        io.emit("excluir_documento_sucesso", nome);
+      }
+    });
+    socket.on("disconnect",()=>{
+      removerConexao(nomeDocumento,nomeUsuario);
+      const usuariosNoDocumento = obterUsuariosNoDocumento(nomeDocumento)
+      console.log(usuariosNoDocumento);
+      io.to(nomeDocumento).emit("usuarios_no_documento", usuariosNoDocumento);
+      
+    });
+  }
+);
 
-  socket.on("texto_editor", async ({ texto, nomeDocumento }) => {
-    const atualizacao = await atualizaDocumento(nomeDocumento, texto);
 
-    if (atualizacao.modifiedCount) {
-      socket.to(nomeDocumento).emit("texto_editor_clientes", texto);
-    }
-  });
 
-  socket.on("excluir_documento", async (nome) => {
-    const resultado = await excluirDocumento(nome);
 
-    if (resultado.deletedCount) {
-      io.emit("excluir_documento_sucesso", nome);
-    }
-  });
 }
 
 export default registrarEventosDocumento;
